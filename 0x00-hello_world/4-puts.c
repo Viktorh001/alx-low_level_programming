c5f34b1905252ab4d09ba3a67b94c1919cef5155
ezuvictor <ezuvictor@gmail.com>
2023-03-18 21:43:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-
-/**
- * main - prints "Programming is like building a multilingual puzzle'followed by a newline."
- * return 0.
- */
-
-int main(void){ puts("\"Programming is like building a multilingual puzzle");return(0);}
+int main() {
+    char str[] = "Programming is like building a multilingual puzzle\n";
+    puts(str);
+    return 0;
+}
