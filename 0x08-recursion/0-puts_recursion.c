2489d52f9410485bf03ab2b640bf0b3a566c5324
ezuvictor <ezuvictor@gmail.com>
2023-04-06 01:09:44 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: the string to be printed.
- * Return: Always 0 (success)
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
@@ -15,3 +15,4 @@ void _puts_recursion(char *s)
 	else
 		_putchar('\n');
 }
+
