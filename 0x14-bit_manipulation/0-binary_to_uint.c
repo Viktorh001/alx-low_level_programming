ed7624654c903de757591582de53ad1c648f0664
ezuvictor <ezuvictor@gmail>
2023-07-08 21:45:01 -0700

    alx bit_manipulation project

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * Converts a binary number to an unsigned int.
+ * binary_to_uint - changes a binary digit to unsigned int
+ * @b: string that carries the binary num
  *
- * @param b Pointer to a string of 0 and 1 chars representing the binary number.
- * @return The converted unsigned int value. Returns 0 if there is an invalid character or if b is NULL.
+ * Return: the converted number
  */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec_val = 0;
 
-unsigned int binary_to_uint(const char *b) {
-    if (b == NULL) {
-        return 0;
-    }
+	if (!b)
+		return (0);
 
-    unsigned int result = 0;
-    int index = 0;
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-    while (b[index] != '\0') {
-        if (b[index] != '0' && b[index] != '1') {
-            return 0;  // Invalid character, return 0
-        }
-
-        result = result * 2 + (b[index] - '0');
-        index++;
-    }
-
-    return result;
-}
-
-int main() {
-    const char *binary = "101010";
-    unsigned int decimal = binary_to_uint(binary);
-
-    printf("%u\n", decimal);  // Output: 42
-
-    return 0;
+	return (dec_val);
 }
